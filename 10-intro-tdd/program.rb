43856348cfb8e1a97f620519d31009bd2606f9c2
Andrew Cohn <andrewcohn0@gmail.com>
2018-06-21 15:05:37 -0400

    post lecture; refactored solutions

--- 10-intro-tdd/program.rb
+++ 10-intro-tdd/program.rb
@@ -1,5 +1,48 @@
+require 'pry'
+# WORKS but would like to refactor:
+# def is_palindrome?(word)
+#   if word.class != String
+#     raise ArgumentError
+#   elsif word == ""
+#     false
+#   else
+#     word = word.gsub(/[^À-ÿa-zA-Z0-9]/, "")
+#     puts word
+#     word.downcase == word.downcase.reverse
+#   end
+# end
+
+# REFACTORED:
+
+
+
 def is_palindrome?(word)
-  # 'dad'
-  word = word.split(' ').join('')
-  word.downcase == word.downcase.reverse
+  raise ArgumentError if word.class != String
+  return false if word.empty?
+
+  sanitized_word = word.gsub(/[^À-ÿA-Za-z0-9]/, "")
+  sanitized_word.downcase == sanitized_word.downcase.reverse
+end
+
+#
+# def factorial(n)
+#   # 4
+#   # 3
+#   # 2
+#   if n == 1 || n == 0
+#     1
+#   else
+#     # return 4 * the return val of factorial(4 -1)
+#     # return 4 * 3 * return val of 2
+#     # return 4 * 3 * 2 * 1
+#     n * factorial(n-1)
+#   end
+#   # 4 -> 4 * 3 * 2 * 1 => 24
+# end
+
+# REFACTORED FACTORIAL
+
+def factorial(n)
+  raise ArgumentError if n > 100000
+  n < 2 ? 1 : n * factorial(n - 1)
 end
--- 10-intro-tdd/spec/program_spec.rb
+++ 10-intro-tdd/spec/program_spec.rb
@@ -14,10 +14,46 @@ describe "is_palindrome?" do
   end
 
   it "should not care about numbers in a string" do
-    expect(is_palindrome?("4114")).to be true
+    expect(is_palindrome?("d41a14d")).to be true
+  end
+
+  it "should return false for an empty string" do
+    expect(is_palindrome?("")).to be false
   end
 
   it "should not care about spaces" do
     expect(is_palindrome?("a man a plan a canal panama")).to be true
+  end
+
+  it "should not care about punctuation" do
+    expect(is_palindrome?("**;;racecar;;;()()%%^%&@#")).to be true
+  end
+
+  it "should not care about special characters" do
+    expect(is_palindrome?('a mán a plän a canäl panáma')).to be true
+  end
+
+  it "should raise an error if given an integer" do
+    expect { is_palindrome?(4) }.to raise_error(ArgumentError)
+  end
+end
+
+describe "factorial" do
+  it "should return the correct value" do
+    expect(factorial(4)).to eq(24)
+    expect(factorial(3)).to eq(6)
+    expect(factorial(15)).to eq(1307674368000)
+  end
+  it "should return 1 for factorial 0" do
+    expect(factorial(1)).to eq(1)
+  end
+  it "should return 1 for factorial 1" do
+    expect(factorial(1)).to eq(1)
+  end
+  it "should raise an ArgumentError when given a non integer argument" do
+    expect { factorial('INVALID STRING INPUT') }.to raise_error(ArgumentError)
+  end
+  it "should raise an ArgumentError when given an integer too large" do
+    expect { factorial(10000000)}.to raise_error(ArgumentError)
   end
 end
