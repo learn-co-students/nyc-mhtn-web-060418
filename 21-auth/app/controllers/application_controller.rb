f0e33289aa363c3b618273935d2e99a47bf55de8
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2018-07-10 11:26:18 -0400

    auth part 1

--- 21-auth/app/controllers/application_controller.rb
+++ 21-auth/app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
 
+  before_action :get_logged_in_user
+
+  private
+  def log_in_user(user_id)
+    session[:user_id] = user_id
+  end
+
+  def logged_in_user_id
+    session[:user_id]
+  end
+
+  def get_logged_in_user
+    @logged_in_user = User.find(logged_in_user_id) if logged_in_user_id
+  end
   
 end
--- 21-auth/app/controllers/users_controller.rb
+++ 21-auth/app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in_user(@user.id)
       redirect_to secrets_path, notice: 'User was successfully created.'
     else
       render :new
