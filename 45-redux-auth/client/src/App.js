51ea8773c96939ad437efeb588a4b84553749e40
Andrew Cohn <andrewcohn0@gmail.com>
2018-08-29 09:57:03 -0400

    nav working; need to cleanup withAuth to redirect to profile if logged in

--- 45-redux-auth/client/src/App.js
+++ 45-redux-auth/client/src/App.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import { Route, NavLink } from 'react-router-dom'
+import React, { Fragment } from 'react'
+import { Route, NavLink, Switch, Redirect, withRouter } from 'react-router-dom'
 import Profile from './components/profile'
 import LoginForm from './components/loginForm'
+import Nav from './components/nav'
 import './App.css'
 
-const App = () => {
+const App = props => {
+  console.log('%c APP PROPS', props, 'color: red')
   return (
-    <div>
-      <NavLink to="/login">Login</NavLink>
-      <NavLink to="/profile">Profile</NavLink>
-      <Route exact path="/profile" component={Profile} />
-      <Route exact path="/login" component={LoginForm} />
-    </div>
+    <Fragment>
+      <Nav />
+      <Switch>
+        {/* TODO: if we're logged in home goes to either profile or login lol */}
+        <Route exact path="/" render={() => <Redirect to="/login" />} />
+
+        <Route exact path="/profile" component={Profile} />
+        <Route exact path="/login" component={LoginForm} />
+        {/* <Route component={NotFound} /> */}
+      </Switch>
+    </Fragment>
   )
 }
 
-export default App
+export default withRouter(App)
---
+++ 45-redux-auth/client/src/components/nav.js
@@ -0,0 +1,29 @@
+import React, { Fragment } from 'react'
+import { connect } from 'react-redux'
+import { NavLink, withRouter } from 'react-router-dom'
+import { Menu, Image } from 'semantic-ui-react'
+import * as actions from '../actions'
+import withAuth from '../hocs/withAuth'
+
+const Nav = ({ user: { loggedIn }, location: { pathname } }) => {
+  console.log(loggedIn, pathname)
+  return (
+    <Menu pointing secondary>
+      {loggedIn ? (
+        <Fragment>
+          <Menu.Item as={NavLink} to="/profile" name="Profile" active={pathname === '/profile'} />
+          <Menu.Menu position="right">
+            {/* TODO: logout */}
+            {/* <Menu.Item to="/logout" name="Logout" onClick={logout} /> */}
+          </Menu.Menu>
+        </Fragment>
+      ) : (
+        <Menu.Item as={NavLink} to="/login" name="Login" active={pathname === '/login'} />
+      )}
+    </Menu>
+  )
+}
+
+const mapStateToProps = ({ usersReducer: user }) => ({ user })
+
+export default withRouter(connect(mapStateToProps)(Nav))
