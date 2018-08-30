3b3a3d27950dda35e0bc3434fd0e115dd49a1ea5
Andrew Cohn <andrewcohn0@gmail.com>
2018-08-30 18:38:35 -0400

    add validation for unique, case insensitive usernames

--- 45-redux-auth/server/app/models/user.rb
+++ 45-redux-auth/server/app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   has_secure_password #=> gives me User#authenticate(plaintext_password) method
-  validates :username, presence: { case_sensitive: false }
+  validates :username, uniqueness: { case_sensitive: false }
 end
