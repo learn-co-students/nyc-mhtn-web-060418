94633839566c21e03bce3b2af8b3a4218f8e3d1e
Andrew Cohn <andrewcohn0@gmail.com>
2018-08-30 21:33:03 -0400

    404 not found lol

--- 45-redux-auth/client/src/components/notFound.js
+++ 45-redux-auth/client/src/components/notFound.js
@@ -1,8 +1,11 @@
 import React, { Fragment } from 'react'
-import { Image } from 'semantic-ui-react'
+import { Header, Image } from 'semantic-ui-react'
 
 const NotFound = () => (
   <Fragment>
+    <Header size="huge" inverted color="purple">
+      Page Not Found lol
+    </Header>
     <Image src="https://thumbs.dreamstime.com/z/senior-man-shrugging-his-shoulders-8775727.jpg" />
   </Fragment>
 )
